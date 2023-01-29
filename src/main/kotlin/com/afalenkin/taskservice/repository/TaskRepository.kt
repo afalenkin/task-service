0910b322807872facb9f49d609cdb17ee2c19063
AlenkinAndrew <oxqq@ya.ru>
2023-01-29 19:39:30 +0300

    repository

---
+++ src/main/kotlin/com/afalenkin/taskservice/repository/TaskRepository.kt
@@ -0,0 +1,26 @@
+package com.afalenkin.taskservice.repository
+
+import com.afalenkin.taskservice.model.Task
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+
+/**
+ *@author Alenkin Andrew
+ *oxqq@ya.ru
+ */
+@Repository
+interface TaskRepository : JpaRepository<Task, Long> {
+
+    fun findTaskById(id: Long): Task
+
+    @Query(value = "SELECT t FROM Task t where t.isOpen = TRUE")
+    fun findAllOpenTasks(): List<Task>
+
+    @Query(value = "SELECT t FROM Task t where t.isOpen = FALSE")
+    fun findAllClosedTasks(): List<Task>
+
+    @Query(value = "SELECT CASE WHEN COUNT(t > 0) THEN TRUE ELSE FALSE END FROM Task t WHERE t.description =: description")
+    fun checkDescriptionUnique(@Param("description") description: String): Boolean
+}
