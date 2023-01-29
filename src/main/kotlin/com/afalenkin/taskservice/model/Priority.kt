acb5e48f5bc1f87a6bdaf2d35c56c209c2c42b51
AlenkinAndrew <oxqq@ya.ru>
2023-01-29 19:01:56 +0300

    Task entity

---
+++ src/main/kotlin/com/afalenkin/taskservice/model/Priority.kt
@@ -0,0 +1,5 @@
+package com.afalenkin.taskservice.model
+
+enum class Priority {
+    LOW, MEDIUM, HIGH
+}
---
+++ src/main/kotlin/com/afalenkin/taskservice/model/Task.kt
@@ -0,0 +1,38 @@
+package com.afalenkin.taskservice.model
+
+import jakarta.persistence.*
+import jakarta.validation.constraints.NotBlank
+import java.time.LocalDateTime
+
+/**
+ *@author Alenkin Andrew
+ *oxqq@ya.ru
+ */
+@Entity
+@Table(
+    name = "task",
+    uniqueConstraints = [UniqueConstraint(name = "uk_task_description", columnNames = ["description"])]
+)
+data class Task(
+    @Id
+    @GeneratedValue(generator = "task_sequence", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "task_sequence", sequenceName = "task_sequence", allocationSize = 1)
+    val id: Long,
+
+    @NotBlank
+    @Column(name = "description", nullable = false, unique = true)
+    var description: String,
+
+    @Column(name = "has_remind", nullable = false)
+    var hasReminder: Boolean,
+
+    @Column(name = "is_open", nullable = false)
+    var isOpen: Boolean,
+
+    @Column(name = "created", nullable = false)
+    val created: LocalDateTime = LocalDateTime.now(),
+
+    @Column(name = "priority", nullable = false)
+    @Enumerated(EnumType.STRING)
+    var priority: Priority,
+)
