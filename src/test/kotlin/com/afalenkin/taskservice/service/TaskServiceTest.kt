fd769d4614ee361b42a1aca66c00b344ae5ae60c
AlenkinAndrew <oxqq@ya.ru>
2023-01-30 22:16:55 +0300

    service tests

--- src/test/kotlin/com/afalenkin/taskservice/service/TaskServiceTest.kt
+++ src/test/kotlin/com/afalenkin/taskservice/service/TaskServiceTest.kt
@@ -1,21 +1,26 @@
 package com.afalenkin.taskservice.service
 
 import com.afalenkin.taskservice.dto.TaskCreateRequest
+import com.afalenkin.taskservice.dto.TaskUpdateRequest
+import com.afalenkin.taskservice.exceptions.TaskNotFoundException
 import com.afalenkin.taskservice.model.Priority
 import com.afalenkin.taskservice.model.Task
 import com.afalenkin.taskservice.repository.TaskRepository
+import com.afalenkin.taskservice.testdata.createClosedTask
 import com.afalenkin.taskservice.testdata.createDtoForTask
-import com.afalenkin.taskservice.testdata.createTask
+import com.afalenkin.taskservice.testdata.createOpenTask
 import io.mockk.MockKAnnotations
+import io.mockk.called
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.slot
+import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 
 /**
@@ -42,7 +47,7 @@ internal class TaskServiceTest {
 
     @Test
     fun `get All tasks`() {
-        val expectedList = listOf(createTask(1), createTask(2))
+        val expectedList = listOf(createOpenTask(1), createOpenTask(2))
         every { mockRepo.findAll() } returns expectedList
         val actualList = service.getAll()
         assertThat(actualList.size).isEqualTo(expectedList.size)
@@ -50,7 +55,7 @@ internal class TaskServiceTest {
 
     @Test
     fun `create task`() {
-        every { mockRepo.save(any()) } returns createTask(1)
+        every { mockRepo.save(any()) } returns createOpenTask(1)
         every { mockRepo.descriptionIsNotUnique(any()) } returns false
         val actual = service.create(TaskCreateRequest("tst", true, true, Priority.HIGH))
         assertThat(actual).isEqualTo(createDtoForTask(1))
@@ -58,17 +63,50 @@ internal class TaskServiceTest {
 
     @Test
     fun `get All Open tasks`() {
+        val expectedList = listOf(createOpenTask(1), createOpenTask(2))
+        every { mockRepo.findAllOpenTasks() } returns expectedList
+        val actualList = service.getAllOpen()
+        assertThat(actualList.size).isEqualTo(expectedList.size)
     }
 
     @Test
     fun `get All Closed tasks`() {
+        val expectedList = listOf(createClosedTask(1), createClosedTask(2))
+        every { mockRepo.findAllClosedTasks() } returns expectedList
+        val actualList = service.getAllClosed()
+        assertThat(actualList.size).isEqualTo(expectedList.size)
     }
 
     @Test
     fun `update task`() {
+        val taskSlot = slot<Task>()
+        every { mockRepo.existsById(any()) } returns true
+        every { mockRepo.save(capture(taskSlot)) } returns createOpenTask(1)
+        every { mockRepo.descriptionIsNotUnique(any()) } returns false
+        val actual = service.update(1L, TaskUpdateRequest("tst", true, true, Priority.HIGH))
+        assertThat(actual).isEqualTo(createDtoForTask(1))
+        verify { mockRepo.save(capture(taskSlot)) }
+    }
+
+    @Test
+    fun `update not foundtask`() {
+        every { mockRepo.existsById(any()) } returns false
+        val ex = assertThrows<TaskNotFoundException> {
+            service.update(
+                1L,
+                TaskUpdateRequest("tst", true, true, Priority.HIGH)
+            )
+        }
+        assertThat(ex.message).isEqualTo("Task with id = 1 is not exists")
+        verify { mockRepo.save(any()) wasNot called }
     }
 
     @Test
     fun `delete task`() {
+        val idSlot = slot<Long>()
+        every { mockRepo.existsById(any()) } returns true
+        every { mockRepo.deleteById(capture(idSlot)) } returns Unit
+        service.delete(1L)
+        verify { mockRepo.deleteById(capture(idSlot)) }
     }
 }
--- src/test/kotlin/com/afalenkin/taskservice/testdata/Preset.kt
+++ src/test/kotlin/com/afalenkin/taskservice/testdata/Preset.kt
@@ -5,7 +5,16 @@ import com.afalenkin.taskservice.model.Priority
 import com.afalenkin.taskservice.model.Task
 import java.time.LocalDate
 
-fun createTask(id: Long) = Task(
+fun createOpenTask(id: Long) = Task(
+    id = 1,
+    description = "test data",
+    hasReminder = true,
+    isOpen = true,
+    created = LocalDate.now().atStartOfDay(),
+    priority = Priority.HIGH
+)
+
+fun createClosedTask(id: Long) = Task(
     id = 1,
     description = "test data",
     hasReminder = true,
